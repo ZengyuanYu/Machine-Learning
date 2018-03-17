c4ddf9a36357fd40ee8dfc2b7e5eaae738493f4f
ZengyuanYu <1952284157@qq.com>
2018-03-17 14:34:56 +0800

    修改一些代码

--- XGBoost/XGBoost预测台风/XGBoost.py
+++ XGBoost/XGBoost预测台风/XGBoost.py
@@ -10,27 +10,27 @@ from sklearn.model_selection import TimeSeriesSplit
 
 
 def GDBTTrain(X, y):
-    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=0)  ##test_size测试集合所占比例
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.5, random_state=0)  ##test_size测试集合所占比例
     test_preds = pd.DataFrame({"label": test_y})
     clf = XGBRegressor(
-        learning_rate=0.1,  # 默认0.3
-        n_estimators=400,  # 树的个数
-        max_depth=8,
+        learning_rate=0.2,  # 默认0.3
+        n_estimators=100,  # 树的个数
+        max_depth=3,
     )
     clf.fit(train_x, train_y)
     test_preds['y_pred'] = clf.predict(test_x)
     stdm = metrics.r2_score(test_preds['label'], test_preds['y_pred'])
 
-    import matplotlib.pyplot as plt  # 画出预测结果图
-    p = test_preds[['label', 'y_pred']].plot(subplots=True, style=['b-o', 'r-*'])
-    plt.plot(test_preds['label'], c = 'blue')
-    plt.plot(test_preds['y_pred'], c ='red')
-    plt.show()
+    # import matplotlib.pyplot as plt  # 画出预测结果图
+    # p = test_preds[['label', 'y_pred']].plot(subplots=True, style=['b-o', 'r-*'])
+    # plt.plot(test_preds['label'], c = 'blue')
+    # plt.plot(test_preds['y_pred'], c ='red')
+    # plt.show()
 
     return stdm, clf
 
 def GDBTest(X, y, clf):
-    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.5, random_state=0)  ##test_size测试集合所占比例
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.1, random_state=0)  ##test_size测试集合所占比例
     test_preds = pd.DataFrame({"label": test_y})
     clf.fit(test_x, test_y)
     test_preds['y_pred'] = clf.predict(test_x)
@@ -61,24 +61,30 @@ def XGTSearch(X, y):
 
 #data = pd.read_csv('X_U4.csv')
 
-Y_list_1 = []
+
 if __name__ == "__main__":
     X_data = pd.read_csv('X_noyear.csv', encoding="gb18030")
-    Y_data = pd.read_csv('Y.csv', encoding="gb18030")
+    Y_data = pd.read_csv('Y_noyear.csv', encoding="gb18030")
     Y_data = Y_data['times']
     out = 'frazee'
     print("输入数据类型：" ,X_data.shape)
     print("标签类型：" ,Y_data.shape)
+    from sklearn import preprocessing
+    min_max_scaler = preprocessing.MinMaxScaler()
     X = X_data.as_matrix()
     y = Y_data.as_matrix()
+    print(X)
+    X = min_max_scaler.fit_transform(X)
+    #train
     stdm, clf = GDBTTrain(X, y)
     print('saving model')
     path = './model/' + out + '_xgb.pkl'
     with open(path, "wb") as f:
         pickle.dump(clf, f)
     print("训练结果",stdm)
+    #test
     path = './model/' + out + '_xgb.pkl'
     with open(path, 'rb') as f:
         model = pickle.load(f)
         stdm = GDBTest(X, y, model)
-        print("测试结果：",stdm)
+        print("测试结果：", stdm)
